fcf0bbc9b0482854c5552578c9201e841a549e55
SergiuToporjinschi <Sergiu.Toporjinschi@gmail.com>
2024-07-10 21:40:10 +0300

    save apps to file

--- gui/screens/apps.go
+++ gui/screens/apps.go
@@ -1,11 +1,14 @@
 package screens
 
 import (
+	"encoding/json"
 	"log/slog"
 	resources "main/assets"
 	"main/devicelayout"
 	"main/monitor"
 	"main/types"
+	"os"
+	"path/filepath"
 	"strings"
 	"sync"
 
@@ -17,6 +20,19 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
+type Application struct {
+	Name    string
+	ExePath string
+}
+
+func (a *Application) Key() string {
+	return a.ExePath
+}
+
+func (a *Application) Value() Application {
+	return *a
+}
+
 type AppsScreen struct {
 	*fyne.Container
 	bndLength     binding.ExternalInt
@@ -24,9 +40,10 @@ type AppsScreen struct {
 	stopChan      chan struct{}
 	closeOnce     sync.Once
 	parentWindow  *fyne.Window
-	appList       types.UniSlice[string]
+	appList       types.UniAnySlice[string, Application]
 	list          *widget.List
 	selectedIndex *int
+	appListMutex  sync.Mutex
 }
 
 func NewAppsScreen(currentDevice *monitor.ConnectedDevice, parentWindow *fyne.Window) NavigationItem {
@@ -36,14 +53,13 @@ func NewAppsScreen(currentDevice *monitor.ConnectedDevice, parentWindow *fyne.Wi
 		bndData:      binding.NewBytes(),
 		Container:    container.NewStack(),
 		parentWindow: parentWindow,
-		appList:      types.NewUniSlice[string](),
+		appList:      loadAppList(),
 	}
 	inst.buildContent(currentDevice.DeviceDescriptor)
 	return inst
 }
 
 func (as *AppsScreen) GetContent() *fyne.Container {
-
 	return as.Container
 }
 
@@ -59,13 +75,14 @@ func (as *AppsScreen) buildContent(_ *devicelayout.DeviceDescriptor) {
 		func() int {
 			return as.appList.Size()
 		},
-		func() fyne.CanvasObject { return widget.NewLabel("test") },
+		func() fyne.CanvasObject { return container.NewHBox(widget.NewLabel("name"), widget.NewLabel("test")) },
 		func(i int, item fyne.CanvasObject) {
-			app := as.appList.Get(i)
-			if app == "" {
+			app := as.appList.GetByIndex(i).Value()
+			if app.ExePath == "" {
 				return
 			}
-			item.(*widget.Label).SetText(app)
+			item.(*fyne.Container).Objects[0].(*widget.Label).SetText(app.Name)
+			item.(*fyne.Container).Objects[1].(*widget.Label).SetText(app.ExePath)
 		})
 	as.list.OnSelected = func(i int) {
 		as.selectedIndex = &i
@@ -80,14 +97,29 @@ func (as *AppsScreen) Destroy() {
 	as.closeOnce.Do(func() {
 		close(as.stopChan)
 	})
-
 }
 
 func (as *AppsScreen) removeApp() {
+	as.appListMutex.Lock()
+	defer as.appListMutex.Unlock()
 	if as.selectedIndex != nil {
 		as.appList.RemoveByIndex(*as.selectedIndex)
 		as.list.Refresh()
+		go as.saveAppList()
 	}
+}
+
+func (as *AppsScreen) addApp(exePaths []string) {
+	as.appListMutex.Lock()
+	defer as.appListMutex.Unlock()
+	for _, exePath := range exePaths {
+		fileName := filepath.Base(exePath)
+		ext := filepath.Ext(fileName)
+		nameWithoutExt := strings.TrimSuffix(fileName, ext)
+		as.appList.Add(&Application{Name: nameWithoutExt, ExePath: exePath})
+	}
+	as.list.Refresh()
+	go as.saveAppList()
 }
 
 func (as *AppsScreen) selectExe() {
@@ -105,13 +137,8 @@ func (as *AppsScreen) selectExe() {
 	dia.Show()
 }
 
-func (as *AppsScreen) addApp(exePaths []string) {
-	as.appList.AddAll(exePaths...)
-	as.list.Refresh()
-}
-
 func (as *AppsScreen) selectProcess() {
-	dia := NewSelectProcessDialog(types.NewStringSetWithValues(as.appList...), as.parentWindow)
+	dia := NewSelectProcessDialog(types.NewStringSetWithValues(as.appList.Keys()...), as.parentWindow)
 	dia.SetOnClose(func(selection types.StringSet, confirmed bool) {
 		if confirmed {
 			slog.Debug("Selected processes: ", "list", dia.GetSelection())
@@ -121,5 +148,53 @@ func (as *AppsScreen) selectProcess() {
 		}
 	})
 	dia.Show()
+}
 
+func loadAppList() types.UniAnySlice[string, Application] {
+	var appList = types.NewUniAnySlice[string, Application]()
+	content, err := os.ReadFile("apps.json")
+	if os.IsNotExist(err) {
+		slog.Warn("File not found: ", "error", err)
+		return appList
+	} else if err != nil {
+		slog.Error("Error opening file: ", "error", err)
+		return appList
+	}
+	var apps []Application
+	err = json.Unmarshal(content, &apps)
+	if err != nil {
+		slog.Error("Error unmarshalling app list: ", "error", err)
+		return appList
+	}
+	pairs := make([]types.PairKeyValue[string, Application], len(apps))
+	for i, app := range apps {
+		pairs[i] = &app
+	}
+	appList.AddAll(pairs...)
+	return appList
+
+}
+
+func (as *AppsScreen) saveAppList() {
+	slog.Debug("Saving app list started")
+	as.appListMutex.Lock()
+	defer as.appListMutex.Unlock()
+	jsonApps, err := json.MarshalIndent(as.appList, "", "  ")
+	if err != nil {
+		slog.Error("Error marshalling app list: ", "error", err)
+		return
+	}
+
+	file, err := os.OpenFile("apps.json", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
+	if err != nil {
+		slog.Error("Error opening file: ", "error", err)
+		return
+	}
+	defer file.Close()
+	_, err = file.WriteString(string(jsonApps))
+	if err != nil {
+		slog.Error("Error writing to file: ", "error", err)
+		return
+	}
+	slog.Debug("Saving app list finished")
 }
---
+++ types/uniAnySlicer.go
@@ -0,0 +1,102 @@
+package types
+
+type PairKeyValue[K StringOrIntFloat, V any] interface {
+	Key() K
+	Value() V
+}
+
+type UniAnySlice[T StringOrIntFloat, V any] []PairKeyValue[T, V]
+
+func NewUniAnySlice[T StringOrIntFloat, V any]() UniAnySlice[T, V] {
+	return []PairKeyValue[T, V]{}
+}
+
+func NewUniAnySliceWithValues[T StringOrIntFloat, V any](values ...PairKeyValue[T, V]) UniAnySlice[T, V] {
+	return append([]PairKeyValue[T, V]{}, values...)
+}
+
+func (s *UniAnySlice[T, V]) Add(element PairKeyValue[T, V]) {
+	for _, item := range *s {
+		if item.Key() == element.Key() {
+			return
+		}
+	}
+	*s = append(*s, element)
+}
+
+func (s *UniAnySlice[T, V]) AddAll(elements ...PairKeyValue[T, V]) {
+	for _, item := range *s {
+		for _, element := range elements {
+			if item.Key() == element.Key() {
+				continue
+			}
+		}
+	}
+	*s = append(*s, elements...)
+}
+
+func (s *UniAnySlice[T, V]) RemoveByIndex(index int) {
+	if index < 0 || index >= len(*s) {
+		return
+	}
+	*s = append((*s)[:index], (*s)[index+1:]...)
+}
+
+func (s *UniAnySlice[T, V]) Remove(element PairKeyValue[T, V]) {
+	for i, item := range *s {
+		if item.Key() == element.Key() {
+			s.RemoveByIndex(i)
+			return
+		}
+	}
+}
+
+func (s *UniAnySlice[T, V]) Size() int {
+	return len(*s)
+}
+
+func (s *UniAnySlice[T, V]) IsEmpty() bool {
+	return s.Size() == 0
+}
+
+func (s *UniAnySlice[T, V]) Clear() {
+	*s = []PairKeyValue[T, V]{}
+}
+
+func (s *UniAnySlice[T, V]) Contains(element PairKeyValue[T, V]) bool {
+	for _, item := range *s {
+		if item.Key() == element.Key() {
+			return true
+		}
+	}
+	return false
+}
+
+func (s *UniAnySlice[T, V]) Values() []V {
+	values := make([]V, 0, len(*s))
+	for _, value := range *s {
+		values = append(values, value.Value())
+	}
+	return values
+}
+
+func (s *UniAnySlice[T, V]) Keys() []T {
+	keys := make([]T, 0, len(*s))
+	for _, item := range *s {
+		keys = append(keys, item.Key())
+	}
+	return keys
+}
+
+func (s *UniAnySlice[T, V]) GetByIndex(index int) PairKeyValue[T, V] {
+	return (*s)[index]
+}
+
+func (s *UniAnySlice[T, V]) GetByKey(key T) PairKeyValue[T, V] {
+	for _, item := range *s {
+		if item.Key() == key {
+			return item
+		}
+	}
+	return nil
+}
